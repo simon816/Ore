9bc5e615dd1d9ab2f5833a08a9fa89642569c3bf
Walker Crouse <walkercrouse@hotmail.com>
2016-04-16 16:04:31 -0400

    Copy config on deploy
    Signed-off-by: Walker Crouse <walkercrouse@hotmail.com>

--- app/controllers/Secured.scala
+++ app/controllers/Secured.scala
@@ -4,7 +4,7 @@ import models.project.Project
 import models.user.User
 import ore.Statistics
 import ore.permission.Permission
-import ore.permission.scope.{Scope, GlobalScope, ScopeSubject}
+import ore.permission.scope.{GlobalScope, ScopeSubject}
 import play.api.mvc.Results._
 import play.api.mvc._
 
