b39f78605bbc1feaaa0ee098af82d444a7af3df7
Walker Crouse <walkercrouse@hotmail.com>
2016-04-24 13:33:37 -0400

    Fix weird whitespace in page editor
    Signed-off-by: Walker Crouse <walkercrouse@hotmail.com>

--- public/javascripts/pageEdit.js
+++ public/javascripts/pageEdit.js
@@ -1,7 +1,21 @@
+var KEY_ENTER = 13;
+
 var onNewPage = function(pageName) {
 };
 
 $(function() {
+    var modal = $('#new-page');
+    modal.on('shown.bs.modal', function() {
+        $(this).find('input').focus();
+    });
+
+    modal.find('input').keydown(function(event) {
+        if (event.keyCode === KEY_ENTER) {
+            event.preventDefault();
+            $('#continue-page').click();
+        }
+    });
+
     $('#continue-page').click(function() {
         onNewPage($('#page-name').val())
     })
