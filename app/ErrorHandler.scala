9c393b4672eb3bb1f5996757f7b84fee0dacfcdf
Walker Crouse <walkercrouse@hotmail.com>
2016-05-03 16:51:05 -0400

    Custom error page
    Signed-off-by: Walker Crouse <walkercrouse@hotmail.com>

---
+++ app/ErrorHandler.scala
@@ -0,0 +1,26 @@
+import javax.inject._
+
+import play.api.http.DefaultHttpErrorHandler
+import play.api._
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc._
+import play.api.mvc.Results._
+import play.api.routing.Router
+
+import scala.concurrent._
+
+class ErrorHandler @Inject()(env: Environment,
+                             config: Configuration,
+                             sourceMapper: OptionalSourceMapper,
+                             router: Provider[Router],
+                             override val messagesApi: MessagesApi)
+                             extends DefaultHttpErrorHandler(env, config, sourceMapper, router)
+                             with I18nSupport {
+
+  override def onProdServerError(request: RequestHeader, exception: UsefulException) = {
+    implicit val req = request
+    implicit val session = request.session
+    Future.successful(InternalServerError(views.html.error(exception.getMessage)))
+  }
+
+}
