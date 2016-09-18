cd69e22cc417473180552f14f096cf3b488c4285
Walker Crouse <walkercrouse@hotmail.com>
2016-09-17 21:37:50 -0400

    Fix occasional NoSuchElementException
    Signed-off-by: Walker Crouse <walkercrouse@hotmail.com>

--- app/controllers/Users.scala
+++ app/controllers/Users.scala
@@ -50,7 +50,7 @@ class Users @Inject()(val fakeUser: FakeUser,
       // Decode SSO payload received from forums and get Ore user
       val user = this.auth.authenticate(sso.get, sig.get)
       this.config.debug(user.username + " has " + user.notifications.size + " notifications.", -1)
-      this.redirectBack(request.flash.get("url").get, user.username)
+      this.redirectBack(request.flash.get("url").getOrElse("/"), user.username)
     }
   }
 
