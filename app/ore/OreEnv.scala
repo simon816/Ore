08e96ab44cf0c7d33c53edcf168514de4af0e92a
Walker Crouse <walkercrouse@hotmail.com>
2016-12-20 19:45:40 -0500

    Make uploads dir configurable
    Signed-off-by: Walker Crouse <walkercrouse@hotmail.com>

--- app/ore/OreEnv.scala
+++ app/ore/OreEnv.scala
@@ -1,5 +1,6 @@
 package ore
 
+import java.nio.file.Paths
 import javax.inject.Inject
 
 import play.api.Environment
@@ -7,12 +8,12 @@ import play.api.Environment
 /**
   * Helper class for getting commonly used Paths.
   */
-final class OreEnv @Inject()(val env: Environment) {
+final class OreEnv @Inject()(val env: Environment, config: OreConfig) {
 
   lazy val root    =  this.env.rootPath.toPath
   lazy val public  =  this.root.resolve("public")
   lazy val conf    =  this.root.resolve("conf")
-  lazy val uploads =  this.root.resolve("uploads")
+  lazy val uploads =  Paths.get(this.config.app.getString("uploadsDir").get)
   lazy val plugins =  this.uploads.resolve("plugins")
   lazy val tmp     =  this.uploads.resolve("tmp")
 
