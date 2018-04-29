7ef04377f6b31f738887e88d3ed9255613051569
Aaron Hill <aa1ronham@gmail.com>
2018-04-29 12:15:53 -0400

    Remove unnecessary Filters class

--- app/Filters.scala
+++
@@ -1,8 +0,0 @@
-import javax.inject.Inject
-import play.api.http.{DefaultHttpFilters, EnabledFilters}
-import play.filters.csrf.CSRFFilter
-import play.filters.headers.SecurityHeadersFilter
-import security.NonceFilter
-
-class Filters @Inject()(enabledFilters: EnabledFilters, csrfFilters: CSRFFilter, securityHeadersFilter: SecurityHeadersFilter, nonceFilter: NonceFilter)
-  extends DefaultHttpFilters(Seq(csrfFilters, securityHeadersFilter, nonceFilter) ++ enabledFilters.filters : _*)
